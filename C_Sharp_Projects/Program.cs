1a08a8913b8332eb9289607a6ff5a605e4babe04
Umed Mirzoev <mirzoev.umed94@gmail.com>
2023-03-23 13:01:40 +0500

    Changed code to OOP

--- C_Sharp_Projects/Program.cs
+++ C_Sharp_Projects/Program.cs
@@ -21,7 +21,10 @@ namespace InstallmentPaymentProgram
                 Console.WriteLine("Enter installment range (in months): ");
                 int rangeMonths = int.Parse(Console.ReadLine());
 
-                decimal totalAmount = CalculateInstallmentPayment(product, amount, phoneNumber, rangeMonths);
+                // Create an instance of the Purchase class and use it to calculate the total payment amount
+                Purchase purchase = new Purchase(product, amount, phoneNumber, rangeMonths);
+                decimal totalAmount = purchase.CalculateInstallmentPayment();
+
                 Console.WriteLine($"Total amount: {totalAmount} somoni");
             }
             catch (Exception ex)
@@ -31,8 +34,27 @@ namespace InstallmentPaymentProgram
 
             Console.ReadLine();
         }
+    }
 
-        static decimal CalculateInstallmentPayment(string product, decimal amount, string phoneNumber, int rangeMonths)
+    class Purchase
+    {
+        // Define properties for the Purchase class
+        public string Product { get; set; }
+        public decimal Amount { get; set; }
+        public string PhoneNumber { get; set; }
+        public int RangeMonths { get; set; }
+
+        // Constructor for the Purchase class
+        public Purchase(string product, decimal amount, string phoneNumber, int rangeMonths)
+        {
+            Product = product;
+            Amount = amount;
+            PhoneNumber = phoneNumber;
+            RangeMonths = rangeMonths;
+        }
+
+        // Calculate the total payment amount for the purchase
+        public decimal CalculateInstallmentPayment()
         {
             Dictionary<string, int[]> installmentRange = new Dictionary<string, int[]>
             {
@@ -41,29 +63,29 @@ namespace InstallmentPaymentProgram
                 { "TV", new int[] { 3, 18 } }
             };
 
-            if (!installmentRange.ContainsKey(product))
+            if (!installmentRange.ContainsKey(Product))
             {
                 throw new ArgumentException("Invalid product");
             }
 
-            int[] range = installmentRange[product];
-            if (rangeMonths < range[0] || rangeMonths > range[1])
+            int[] range = installmentRange[Product];
+            if (RangeMonths < range[0] || RangeMonths > range[1])
             {
-                throw new ArgumentException($"Invalid installment range for {product}");
+                throw new ArgumentException($"Invalid installment range for {Product}");
             }
 
             Dictionary<string, decimal> additionalPercent = new Dictionary<string, decimal>
             {
-                { "Smartphone", 0.03M },
-                { "Computer", 0.04M },
-                { "TV", 0.05M }
+                { "Smartphone", 0.03m },
+                { "Computer", 0.04m },
+                { "TV", 0.05m }
             };
 
-            decimal additionalPercentage = additionalPercent[product] * (rangeMonths - range[0] + 1);
-            decimal totalAmount = amount * (1 + additionalPercentage);
+            decimal additionalPercentage = additionalPercent[Product] * (RangeMonths - range[0] + 1);
+            decimal totalAmount = Amount * (1 + additionalPercentage);
 
-            string message = $"You have purchased a {product} for {amount} somoni with an installment plan for {rangeMonths} months. The total amount is {totalAmount} somoni.";
-            SendSms(phoneNumber, message);
+            string message = $"You have purchased a {Product} for {Amount} somoni with an installment plan for {RangeMonths} months. The total amount is {totalAmount} somoni.";
+            SendSms(PhoneNumber, message);
 
             return totalAmount;
         }
